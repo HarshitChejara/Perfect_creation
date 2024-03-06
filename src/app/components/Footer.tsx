adfb46d285229d52a68b0ab1de40ea378a25df61
HarshitChejara <harshitchejara29@gmail.com>
2024-03-06 11:08:19 +0530

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
+export default function Footer()
+{
+    return(
+        <React.Fragment>
+        <footer className="text-white body-font bg-[#855439]">
+        <div className="flex flex-col text-center w-full">
+            <h1 className="sm:text-5xl text-2xl mt-8 font-serif font-medium title-font tracking-widest">LETS CHANGE YOUR OWN HOME <br/>INTERIOR DESIGN NOW</h1>
+        </div>
+        <div className="container px-5 py-24 sm:px-14 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+            <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center">
+                <h1 className="text-2xl font-bold">CONTACT US</h1>
+            </a>
+            <p className="mt-5 text-sm"><FontAwesomeIcon icon={faLocationDot} /> &nbsp;Lumbung Hidup East Java</p>
+            <p className="mt-3 text-sm"><FontAwesomeIcon icon={faEnvelope} /> Hello@Homco.com</p>
+            <form action="#">
+              <div className="items-center border-b-4 border-white mx-auto mb-3 mt-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+                  <div className="relative w-full">
+                      <input className="block p-3 text-white w-full text-sm bg-white bg-opacity-30 border border-white dark:bg-white-700 placeholder-white dark:text-white" placeholder="Email Address" type="email" id="email" required/>
+                  </div>
+                  <div>
+                      <button type="submit" className="py-3 px-3 w-full text-sm font-medium text-center text-white border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"><FontAwesomeIcon icon={faArrowRight} /></button>
+                  </div>
+              </div>
+            </form>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-5 justify-center sm:justify-start">
+                <a href="https://facebook.com" target="_blank">
+                    <FontAwesomeIcon icon={faSquareFacebook} className="w-5 h-5" />
+                </a>
+                <a href="https://twitter.com" target="_blank" className="ml-3">
+                    <FontAwesomeIcon icon={faSquareTwitter} className="w-5 h-5" />
+                </a>
+                <a href="https://instagram.com" target="_blank" className="ml-3">
+                    <FontAwesomeIcon icon={faSquareInstagram} className="w-5 h-5" />
+                </a>
+                <a href="https://linkedin.com" target="_blank" className="ml-3">
+                    <FontAwesomeIcon icon={faLinkedin} className="w-5 h-5" />
+                </a>
+            </span>
+            </div>
+            <div className="flex-grow flex flex-wrap md:pl-44 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                <h2 className="title-font tracking-widest text-2xl font-bold mb-3">NAVIGATION</h2>
+                <nav className="list-none mb-10">
+                <li>
+                    <a><FontAwesomeIcon icon={faAngleRight} /> Homepage</a>
+                </li>
+                <li>
+                    <a><FontAwesomeIcon icon={faAngleRight} /> About US</a>
+                </li>
+                <li>
+                    <a><FontAwesomeIcon icon={faAngleRight} /> Our Projects</a>
+                </li>
+                </nav>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                <h2 className="title-font font-medium text-gray-900 tracking-widest text-2xl mb-3"> &nbsp; </h2>
+                <nav className="list-none mb-10">
+                <li>
+                    <a><FontAwesomeIcon icon={faAngleRight} /> Project Details</a>
+                </li>
+                <li>
+                    <a><FontAwesomeIcon icon={faAngleRight} /> Careers</a>
+                </li>
+                </nav>
+            </div>
+            </div>
+        </div>
+        <div className="">
+            <div className="border-t border-white container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-sm text-center sm:text-left">ALLRIGHT RESERVED -
+                <a href="#" rel="noopener noreferrer" className="ml-1" target="_blank">HOMCO INTERIOR</a>
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                <a className="text-sm">DISCLAIMER</a>
+                <a className="ml-16 text-sm">PRIVACY POLICY</a>
+                <a className="ml-16 text-sm">TERM OF USE</a>
+            </span>
+            </div>
+        </div>
+        </footer>
+        </React.Fragment>
+    );
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Link from "next/link";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
+// import Image from "next/image";
+
+const Header = () => {
+  return (
+      <Popover className={"sticky z-50 mx-auto flex items-center sm:px-12 px-6 py-2 h-24"}>
+        {/* <Link href="/"><img src="/logo.png" width="50" height="50"></img></Link> */}
+        <Link href="/"><h1 className="text-black font-bold sm:text-3xl tracking-widest">PERFECT CREATION</h1></Link>
+        <div className="bg-transparent md:ml-auto">
+          <div className="hidden sm:flex items-center justify-center gap-2 md:gap-8">
+            <Link href="about" className="tracking-wide text-black font-bold sm:mr-14">HOME</Link>
+            <Link href="features" className="tracking-wide text-black font-bold sm:mr-14">ABOUT &nbsp;US</Link>
+            <Link href="testimonial" className="tracking-wide text-black font-bold sm:mr-14">OUR &nbsp;PROJECTS</Link>
+            <Link href="blog" className="tracking-wide text-black font-bold sm:mr-14">CAREERS</Link>
+            <Link href="early_access" className="tracking-wide text-black font-bold">CONTACT US</Link>
+          </div>
+        </div>
+
+        <div className="flex grow items-center justify-end sm:hidden">
+          <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500
+          focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
+            <span className="sr-only">Open menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </Popover.Button>
+        </div>
+        <Popover.Overlay className="sm:hidden fixed inset-0 bg-black opacity-30"/>
+
+        <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95">
+
+        <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden z-50">
+          <div className="rounded-lg bg-white shadow-lg ring-1 ring-black ring-opaity-5 divide-y-2 divide-gray-50">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex item-center justify-between">
+                <h1 className="text-black font-bold text-xl">PERFECT CREATION</h1>
+                <div className="-mr-2">
+                <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500
+                focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  <Link className="text-black focus:outline-none px-2 font-bold" href="about">HOME</Link>
+                  <Link className="text-black focus:outline-none px-2 font-bold" href="features">ABOUT &nbsp;US</Link>  
+                  <Link className="text-black focus:outline-none px-2 font-bold" href="testimonial">OUR &nbsp;PROJECTS</Link>
+                  <Link className="text-black focus:outline-none px-2 font-bold" href="blog">CAREERS</Link>  
+                  <Link className="text-black focus:outline-none px-2 font-bold" href="early_access">CONTACT US</Link>
+                </nav>
+              </div>
+              
+            </div>
+          </div>
+        </Popover.Panel>
+        </Transition>
+  
+        {/* <div className="hidden sm:px-12 sm:block px-6 py-7 h-24">
+        <Link href="contact"><button type="button" className="text-white rounded py-2 px-8 leading-6 bg-[#303030]">Contact</button></Link>
+        </div> */}
+      </Popover>
+  );
+};
+export default Header;
